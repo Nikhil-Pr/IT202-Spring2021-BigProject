a0a3944d56d9153b0a8703ce8d6f5a590d51e724
Nikhil-Pr <nprasad493@gmail.com>
2021-04-24 20:07:40 -0500

    Current season functionality

--- src/main.js
+++ src/main.js
@@ -1,15 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
-import router from './router'
-
+import router from './router';
+import VueApexCharts from 'vue-apexcharts';
 Vue.config.productionTip = false
 
 new Vue({
   vuetify,
   router,
+  VueApexCharts,
   render: h => h(App)
 }).$mount('#app')
+
+Vue.component('apexchart', VueApexCharts)
 
 router.replace('/')
 document.title = 'Formula Stats'
--- src/views/CurrentSeason.vue
+++ src/views/CurrentSeason.vue
@@ -5,24 +5,90 @@
       Current Season Statistics
     </div>
     <div>
-
+      <v-data-table  dense dark :headers="headers" :items="driverStandings" :items-per-page="10" class=" mt-5 elevation-2">
+        <template #item.full_name="{ item }">{{ item.Driver.givenName }} {{ item.Driver.familyName }}</template>
+      </v-data-table>
+    </div>
+    <div class="mt-5 display-1 font-weight-bold white--text">
+      Driver Wins
+    </div>
+    <div style="width: 90%">
+      <apexchart height="250" v-if="loaded" type="bar" :options="options" :series="winSeries"></apexchart>
+    </div>
+    <div class="mt-5 display-1 font-weight-bold white--text">
+      Driver Points
+    </div>
+    <div style="width: 90%">
+      <apexchart height="250" v-if="loaded" type="bar" :options="options" :series="pointSeries"></apexchart>
     </div>
   </v-layout>
 </v-container>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   name: "CurrentSeason",
   data: () => ({
-    DriverStandings:[]
+    driverStandings:[],
+    loaded: false,
+
+    options:{
+      chart:{
+        background: '#fff',
+          width: '100%'
+      },
+      xaxis:{
+        categories:[],
+      },
+    },
+
+    pointSeries:[{
+      name: 'point series',
+      data:[]
+    }],
+    winSeries: [{
+      name: 'win series',
+      data: []
+    }],
+    headers:[
+      {
+        text: 'Position',
+        sortable: true,
+        value: 'position'
+      },
+      {
+        text: 'Driver',
+        sortable: false,
+        value: `full_name`
+      },
+      {
+        text: 'Points',
+        sortable: false,
+        value: 'points'
+      },
+      {
+        text: 'Wins',
+        sortable: false,
+        value: 'wins'
+      }
+    ]
   }),
   created() {
-
+    this.getStandings()
   },
   methods:{
     getStandings(){
-
+      axios.get('http://ergast.com/api/f1/current/driverStandings.json')
+      .then(response => {
+        this.driverStandings = response.data.MRData.StandingsTable.StandingsLists[0].DriverStandings
+        this.winSeries[0].data = response.data.MRData.StandingsTable.StandingsLists[0].DriverStandings.map(ele => parseInt(ele.wins))
+        this.pointSeries[0].data = response.data.MRData.StandingsTable.StandingsLists[0].DriverStandings.map(ele => parseInt(ele.points))
+        this.options.xaxis.categories = response.data.MRData.StandingsTable.StandingsLists[0].DriverStandings.map(ele => ele.Driver.code)
+        console.log(this.winSeries.data)
+        this.loaded = true
+      })
+      .catch(alert)
     }
   }
 
