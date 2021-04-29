ce9f7a8e6fca3e424c86c06e47457d1577444f9c
Nikhil Prasad <nprasad493@gmail.com>
2021-04-28 20:28:36 -0500

    Deploy Test

--- source/vue.config.js
+++ source/vue.config.js
@@ -1,8 +1,11 @@
 module.exports = {
-  transpileDependencies: [
-    'vuetify'
-  ],
-  pwa:{
-    workboxPluginMode: "GenerateSW"
-  }
+    transpileDependencies: [
+        'vuetify'
+    ],
+    pwa: {
+        workboxPluginMode: "GenerateSW"
+    },
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/IT202-Spring2021-BigProject/'
+        : '/'
 }
